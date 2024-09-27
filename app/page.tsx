9e03f11c899cfa4d252bbcc39548f7a6f0a9f3cf
vigneshs11 <vigneshswaminathan11@gmail.com>
2024-09-27 23:02:34 +0530

    Landing Page - commit -2

--- app/page.tsx
+++ app/page.tsx
@@ -6,44 +6,51 @@ export default function Home() {
       <div className="background-container">
       <div className="content">
 
+
       {/* Hero Section */}
       <section className="flex flex-col items-center text-center mt-12">
       <h1
-  className="text-8xl font-bold text-green-400
+  className="text-8xl font-bold text-pink-600
     mb-4
      bg-opacity-50
     w-2xl
     drop-shadow-7xl
     mt-24
+    font-sans
     "
 >
-  Pushti
-</h1>        <p className="mt-4 text-lg text-black w-1/2">
-          Wholesome Protein Rich Vegetarian Meals, that ensure you meet your daily protein intake.
-          Breaking the myth of "No Veg is only protein rich" one meal at a time
+  PUSHTI
+</h1>        <p className="mt-4 text-lg text-white-500 w-1/2 font-bold rounded-lg">
+          Wholesome high protein food, that enables you meet your daily protein intake requirement.
+          Our mission is to change the protein narrative of India one bowl at a time.
         </p>
         
       </section>
 
       {/* Feature/Value Proposition Section */}
-      <section className="grid gap-8 mt-16 text-center sm:grid-cols-2 lg:grid-cols-1">
+      <section className="grid gap-8 mt-16 text-center sm:grid-cols-2 lg:grid-cols-2">
         <div className="p-6 bg-gray-100 rounded-lg ">
-          <h2 className="text-2xl font-semibold text-green-400">Healthy Ingredients</h2>
+          <h2 className="text-2xl font-semibold text-green-400">Are you meeting your protein need?</h2>
           <p className="mt-2 text-black">
-            We source the freshest and healthiest ingredients for every meal.
+            Get 0.8x - 1.5x (gms per body weight in kg) of protein from natural food sources 
           </p>
         </div>
         <div className="p-6 bg-gray-100 rounded-lg">
-          <h2 className="text-2xl font-semibold text-green-400">Fast Delivery</h2>
+          <h2 className="text-2xl font-semibold text-green-400">Non Packaged, Non Processed, No Oil</h2>
           <p className="mt-2 text-black">
-            Coming soon an all major food delivery Apps 
+            We use whole foods that are locally sourced and in measured quantities, with every bowl served, so you know your protein and calorie intake
           </p>
         </div>
         <div className="p-6 bg-gray-100 rounded-lg">
-          <h2 className="text-2xl font-semibold text-green-400">Sustainable Packaging</h2>
+          <h2 className="text-2xl font-semibold text-green-400">Here to be your post work out partner</h2>
           <p className="mt-2 text-black">
-            Our packaging is eco-friendly, minimizing our environmental
-            footprint.
+            Did you know? It is essential to hydrate yourself and consume a nutritious meal within the first 20 minutes of your workout
+          </p>
+        </div>
+        <div className="p-3 bg-gray-100 rounded-lg">
+          <h2 className="text-2xl font-semibold text-green-400">Coming Soon...</h2>
+          <p className="mt-2 text-black">
+            On Swiggy, Zomato and a store near your gym
           </p>
         </div>
       </section>
